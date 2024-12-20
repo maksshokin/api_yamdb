a9cf9ea90a2a84c9c073b464cea31a131525360c
Maksim Shokin <maksshokin90@gmail.com>
2024-12-20 05:07:45 +0300

    Допилил Ауентитификацию

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from reviews.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
@@ -26,3 +26,37 @@ class NotAdminSerializer(serializers.ModelSerializer):
             'role'
         )
         read_only_fields = ('role',)
+
+class SingUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+        )
+
+    def validate(self, data):
+        if User.objects.filter(email=data.get('email')):
+            raise serializers.ValidationError(
+                'Вы уже зарегестрированы!'
+            )
+        if User.objects.filter(username=data.get('username')):
+            raise serializers.ValidationError(
+                'Это имя занято!'
+            )
+        return data
+
+class TokenSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True
+    )
+    confirmation_code = serializers.CharField(
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'confirmation_code'
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,20 +1,24 @@
-from api.serializers import UserSerializer, NotAdminSerializer
+from django.shortcuts import get_object_or_404
+from api.serializers import (
+    AdminSerializer,
+    NotAdminSerializer,
+    SingUpSerializer,
+    TokenSerializer
+)
 from api.permissions import IsSuperUserOrAdmin
 from api_yamdb.settings import EMAIL
 from reviews.models import User
 
-
-from django.core.mail import EmailMessage
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
-from rest_framework import filters, viewsets, status
+from rest_framework import filters, viewsets, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
 class UserViewSet(viewsets.GenericViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = AdminSerializer
     permission_classes = (IsSuperUserOrAdmin,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('username',)
@@ -24,11 +28,11 @@ class UserViewSet(viewsets.GenericViewSet):
         detail=False,
         url_path='me')
     def user_info(self, request):
-        serializer = UserSerializer(request.user)
+        serializer = AdminSerializer(request.user)
         if request.method == 'GET':
             return Response(serializer.data)
         if request.user.is_admin:
-            serializer = UserSerializer(
+            serializer = AdminSerializer(
                 request.user,
                 data=request.data,
                 partial=True
@@ -48,7 +52,38 @@ class UserViewSet(viewsets.GenericViewSet):
 
 
 class TokenViewSet(viewsets.GenericViewSet):
-    pass
+    queryset = User.objects.all()
+    serializer_class = TokenSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data.get('username')
+        confirmation_code = serializer.validated_data.get('confirmation_code')
+        user = get_object_or_404(User, username=username)
+
+        if not default_token_generator.check_token(user, confirmation_code):
+            message = {'confirmation_code': 'Код подтверждения невалиден'}
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        message = {'token': str(AccessToken.for_user(user))}
+        return Response(message, status=status.HTTP_200_OK)
 
 class SingUpViewSet(viewsets.GenericViewSet):
-    pass
+    queryset = User.objects.all()
+    serializer_class = SingUpSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        serializer = SingUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail(
+            from_email=EMAIL,
+            recipient_list=(user.email,),
+            fail_silently=False,
+            subject='Код подтверждения',
+            message=f'Код подтверждения: {confirmation_code}',
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
