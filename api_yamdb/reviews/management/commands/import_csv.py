cf46beaddf5315202c888afa4b469d0910dcec05
Nikita Kuznetsov <nikita.nikolaevitch@gmail.com>
2024-12-25 20:09:13 +0300

    csv fixed

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -1,9 +1,20 @@
 import csv
+import os
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
-from reviews.models import Category, Genre, Title, Review, Comment
+from constants import (
+    CSV_FILES_PATH,
+    USERS_CSV,
+    CATEGORY_CSV,
+    GENRE_CSV,
+    TITLES_CSV,
+    GENRE_TITLE_CSV,
+    REVIEW_CSV,
+    COMMENTS_CSV
+)
+from reviews.models import Category, Comment, Genre, Title, Review
 
 User = get_user_model()
 
@@ -12,7 +23,9 @@ class Command(BaseCommand):
     help = 'Import data from csv files'
 
     def handle(self, *args, **kwargs):
-        with open('static/data/users.csv', encoding='utf-8') as file:
+        with open(
+            os.path.join(CSV_FILES_PATH, USERS_CSV), encoding='utf-8'
+        ) as file:
             reader = csv.DictReader(file)
             for row in reader:
                 User.objects.get_or_create(
@@ -22,40 +35,53 @@ class Command(BaseCommand):
                     role=row['role'],
                     bio=row['bio'],
                     first_name=row['first_name'],
-                    last_name=row['last_name']
+                    last_name=row['last_name'],
                 )
 
-        with open('static/data/category.csv', encoding='utf-8') as file:
+        with open(
+            os.path.join(CSV_FILES_PATH, CATEGORY_CSV), encoding='utf-8'
+        ) as file:
             reader = csv.DictReader(file)
             for row in reader:
                 Category.objects.get_or_create(
                     name=row['name'],
-                    slug=row['slug']
+                    slug=row['slug'],
                 )
 
-        with open('static/data/genre.csv', encoding='utf-8') as file:
+        with open(
+            os.path.join(CSV_FILES_PATH, GENRE_CSV), encoding='utf-8'
+        ) as file:
             reader = csv.DictReader(file)
             for row in reader:
-                Genre.objects.get_or_create(name=row['name'], slug=row['slug'])
+                Genre.objects.get_or_create(
+                    name=row['name'], 
+                    slug=row['slug'],
+                )
 
-        with open('static/data/titles.csv', encoding='utf-8') as file:
+        with open(
+            os.path.join(CSV_FILES_PATH, TITLES_CSV), encoding='utf-8'
+        ) as file:
             reader = csv.DictReader(file)
             for row in reader:
                 category = Category.objects.get(id=row['category'])
                 Title.objects.get_or_create(
                     name=row['name'],
                     year=row['year'],
-                    category=category
+                    category=category,
                 )
 
-        with open('static/data/genre_title.csv', encoding='utf-8') as file:
+        with open(
+            os.path.join(CSV_FILES_PATH, GENRE_TITLE_CSV), encoding='utf-8'
+        ) as file:
             reader = csv.DictReader(file)
             for row in reader:
                 title = Title.objects.get(id=row['title_id'])
                 genre = Genre.objects.get(id=row['genre_id'])
                 title.genre.add(genre)
 
-        with open('static/data/review.csv', encoding='utf-8') as file:
+        with open(
+            os.path.join(CSV_FILES_PATH, REVIEW_CSV), encoding='utf-8'
+        ) as file:
             reader = csv.DictReader(file)
             for row in reader:
                 title = Title.objects.get(id=row['title_id'])
@@ -66,10 +92,12 @@ class Command(BaseCommand):
                     text=row['text'],
                     author=author,
                     score=row['score'],
-                    pub_date=row['pub_date']
+                    pub_date=row['pub_date'],
                 )
 
-        with open('static/data/comments.csv', encoding='utf-8') as file:
+        with open(
+            os.path.join(CSV_FILES_PATH, COMMENTS_CSV), encoding='utf-8'
+        ) as file:
             reader = csv.DictReader(file)
             for row in reader:
                 review = Review.objects.get(id=row['review_id'])
@@ -79,7 +107,9 @@ class Command(BaseCommand):
                     review=review,
                     text=row['text'],
                     author=author,
-                    pub_date=row['pub_date']
+                    pub_date=row['pub_date'],
                 )
 
-        self.stdout.write(self.style.SUCCESS('Successfully imported'))
+        self.stdout.write(
+            self.style.SUCCESS('Successfully imported')
+        )
